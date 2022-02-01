204811d533e56a66f53a0612d73861dcb390ab6e
faroliwi <abad.simon.adrian.smr@gmail.com>
2022-02-01 02:29:47 +0100

    Show sessions

--- resources/views/sesion/show.blade.php
+++ resources/views/sesion/show.blade.php
@@ -22,11 +22,11 @@
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item"><a href="/activities">Activities</a></li>
+            <li class="breadcrumb-item"><a href="/activities">Sessions</a></li>
             <li class="breadcrumb-item active" aria-current="page">Show</li>
         </ol>
     </nav>
-    <h1 class="mt-3 mb-3">{{$activity->name}}</h1>
+    <h1 class="mt-3 mb-3">{{$sesion->name}}</h1>
 
     <table id="table" class="table table-bordered table-hover">
         <thead>
@@ -36,25 +36,30 @@
                     <div class="fht-cell"></div>
                 </th>
                 <th style="" data-field="name">
-                    <div class="th-inner ">Descripción</div>
+                    <div class="th-inner ">Mes</div>
                     <div class="fht-cell"></div>
                 </th>
                 <th style="" data-field="name">
-                    <div class="th-inner ">Duración minutos</div>
+                    <div class="th-inner ">Días</div>
                     <div class="fht-cell"></div>
                 </th>
                 <th style="" data-field="name">
-                    <div class="th-inner ">Nº max participantes</div>
+                    <div class="th-inner ">Hora inicio</div>
+                    <div class="fht-cell"></div>
+                </th>
+                <th style="" data-field="name">
+                    <div class="th-inner ">Hora final</div>
                     <div class="fht-cell"></div>
                 </th>
             </tr>
         </thead>
         <tbody>
             <tr data-index="1">
-                <td>{{$activity->id}}</td>
-                <td>{{$activity->description}} </td>
-                <td>{{$activity->activity_minutes}} </td>
-                <td>{{$activity->max_participants}} </td>
+                <td>{{$sesion->id}}</td>
+                <td>{{$sesion->diasSesion}} </td>
+                <td>{{$sesion->mesSesion}} </td>
+                <td>{{$sesion->horaInicio}} </td>
+                <td>{{$sesion->horaFinal}} </td>
             </tr>
         </tbody>
     </table>
